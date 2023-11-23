5f387f554c2ea5366b32fce07349071a877a7d6d
JorgeVillalobosJ <jlobos28@gmail.com>
2023-11-22 21:49:47 -0500

    Last update

--- Chapter 5/TestAutomobile.java
+++ Chapter 5/TestAutomobile.java
@@ -4,15 +4,12 @@ public class TestAutomobile {
     public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
 
-        // Creating Automobile Objects
         Automobile carOne = new Automobile(4886, "Kia", "Optima", "Silver", 2012, 52608060, 30);
         Automobile carTwo = new Automobile(4560, "Nissan", "Sentra", "Red", 2018, 23134589, 45);
 
-        // Testing Methods
         carOne.displayAutomobile();
         carTwo.displayAutomobile();
         
-        // accelerate and brake
         System.out.println("");
         carOne.accelerate(21);
         carOne.accelerate(21.5);    
